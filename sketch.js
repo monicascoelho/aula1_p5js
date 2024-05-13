8dabaefc1e124aa0eea20925a62e0169a9e6d3da
monicascoelho <168586687+monicascoelho@users.noreply.github.com>
2024-05-13 15:45:36 -0300

    Adicionando os arquivos js e css

--- sketch.js
+++ sketch.js
@@ -1,15 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.1/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.1/addons/p5.sound.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8" />
+let palavra;
 
-  </head>
-  <body>
-    <main>
-    </main>
-    <script src="sketch.js"></script>
-  </body>
-</html>
+function setup() {
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+  
+}
+
+function palavraAleatoria() {
+  
+  let palavras = ["Caminhante", "Caminho", "Caminha"];
+  
+  return random(palavras);
+}
+
+function inicializaCores() {
+  background("white");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function palavraParcial(minimo, maximo) {
+  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
+  let parcial = palavra.substring(0, quantidade);
+  return parcial;
+}
+
+function draw() {
+  
+  inicializaCores();
+
+  let texto = palavraParcial(0, width);
+    
+  text(texto, 200, 200);
+  
+}
+
+function modoNoturno(horario) {
+  if (horario > 18) {
+    console.log("Você precisa ligar o modo escuro!");
+  } else {
+    console.log("Modo noturno não é necessário neste momento.");
+  }
+}
+
+modoNoturno(15);
+modoNoturno(20);
